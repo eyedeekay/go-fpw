c6654d9f7fce2565394926a1fc1f42718a1018c1
idk <hankhill19580@gmail.com>
2022-10-05 02:41:41 -0400

    Don't set a width/height

--- ui.go
+++ ui.go
@@ -320,8 +320,6 @@ func NewFirefox(url, dir string, width, height int, customArgs ...string) (UI, e
 	}
 	args := append(firefoxArgs, "--profile")
 	args = append(args, dir)
-	args = append(args, "--window-size")
-	args = append(args, fmt.Sprintf("%d,%d", width, height))
 	args = append(args, customArgs...)
 	args = append(args, url)
 	//args = append(args, "--remote-debugging-port=0")
