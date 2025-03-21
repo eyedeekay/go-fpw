abaaba21bb4f514b4f5f7c486bdaf631c65d283d
eyedeekay <hankhill19580@gmail.com>
2025-03-20 21:08:51 -0400

    update makefile

---
+++ ffapp/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/url"
+
+	"flag"
+
+	fcw "github.com/eyedeekay/go-fpw"
+)
+
+func main() {
+	starturl := flag.String("url", "https://duckduckgo.com", "URL to open in Firefox")
+	flag.Parse()
+	uri, err := url.Parse(*starturl)
+	if err != nil {
+		log.Println(err)
+	}
+	hasPortable := fcw.PortablePath()
+	if hasPortable != "" {
+		log.Println("This is a portable installation.", hasPortable)
+	} else {
+		log.Println("This is not a portable installation.", hasPortable)
+	}
+	ui, err := fcw.WebAppFirefox(uri.Hostname(), false, *starturl)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(ui.Log())
+	<-ui.Done()
+}
