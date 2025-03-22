57b3c716bd8a81f335af02ee2ab356e8af25abd1
eyedeekay <hankhill19580@gmail.com>
2025-03-22 14:43:52 -0400

    user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,55 @@
+/*** Disable All Telemetry ***/
+
+// Core Telemetry Systems
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+
+// Coverage & Health Reports
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+// Activity Stream / Firefox Home Telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+// Studies
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+// Crash Reports
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+
+/*** Disable First-Run Procedures ***/
+
+// Startup & Welcome Notices
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+
+// Extension Recommendations & Discovery
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+
+// First Run New Tab Page
+user_pref("browser.startup.firstrunSkipsHomepage", true);
+user_pref("browser.startup.homepage_welcome_url", "");
+user_pref("browser.startup.homepage_welcome_url.additional", "");
+
+// Disable What's New Page
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_override_url", "");
+
+// Disable Shopping Experience
+user_pref("browser.shopping.experience2023.enabled", false);
